0a9f6c67f970c0001ed7486e2436b3e29803b60a
drib861204 <drib861204@gmail.com>
2019-10-10 21:46:21 +0800

    version1: 1.data failed, 2.data failed, 3.data successed

--- main.cpp
+++ main.cpp
@@ -10,19 +10,204 @@ void end(int **m,int r,int c){
             cout << m[i][j];
         cout << endl;
     }
-    
-};
-void check(int **m, string block, int base, int row, int &k){
-    
-
-
-
-
-
-
 }
-void deleterow();//TODO
 
+void check(int **m, string block, int b, int r, int &k){
+    if(block=="T1"){
+        if(r==0 || m[r][b]==1 || m[r-1][b+1]==1 || m[r][b+2]==1 ){
+            m[r+1][b]=1; m[r][b+1]=1; m[r+1][b+2]=1; m[r+1][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+   
+    else if(block=="T2"){
+        if(r==0 || m[r][b]==1 || m[r-1][b+1]==1 ){
+            m[r+1][b]=1; m[r][b+1]=1; m[r+1][b+1]=1; m[r+2][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="T3"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 || m[r-1][b+2]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r][b+2]=1; m[r+1][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="T4"){
+        if(r==0 || m[r-1][b]==1 || m[r][b+1]==1 ){
+            m[r][b]=1; m[r+1][b+1]=1; m[r+1][b]=1; m[r+2][b]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="L1"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r+1][b]=1; m[r+2][b]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="L2"){
+        if(r==0 || m[r-1][b]==1 || m[r][b+1]==1 || m[r][b+2]==1 ){
+            m[r][b]=1; m[r+1][b+1]=1; m[r+1][b+2]=1; m[r+1][b]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="L3"){
+        if(r==0 || m[r+1][b]==1 || m[r-1][b+1]==1 ){
+            m[r+2][b]=1; m[r][b+1]=1; m[r+1][b+1]=1; m[r+2][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="L4"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 || m[r-1][b+2]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r][b+1]=1; m[r+1][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="J1"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r+1][b+1]=1; m[r+2][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="J2"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 || m[r-1][b+2]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r][b+2]=1; m[r+1][b]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="J3"){
+        if(r==0 || m[r-1][b]==1 || m[r+1][b+1]==1 ){
+            m[r][b]=1; m[r+2][b+1]=1; m[r+1][b]=1; m[r+2][b]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="J4"){
+        if(r==0 || m[r][b]==1 || m[r][b+1]==1 || m[r-1][b+2]==1 ){
+            m[r+1][b]=1; m[r+1][b+1]=1; m[r][b+2]=1; m[r+1][b+2]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="S1"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 || m[r][b+2]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r+1][b+2]=1; m[r+1][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="S2"){
+        if(r==0 || m[r][b]==1 || m[r-1][b+1]==1 ){
+            m[r+1][b]=1; m[r][b+1]=1; m[r+1][b+1]=1; m[r+2][b]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="Z1"){
+        if(r==0 || m[r][b]==1 || m[r-1][b+1]==1 || m[r-1][b+2]==1 ){
+            m[r+1][b]=1; m[r][b+1]=1; m[r][b+2]=1; m[r+1][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="Z2"){
+        if(r==0 || m[r-1][b]==1 || m[r][b+1]==1 ){
+            m[r][b]=1; m[r+1][b+1]=1; m[r+1][b]=1; m[r+2][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="I1"){
+        if(r==0 || m[r-1][b]==1 ){
+            m[r][b]=1; m[r+1][b]=1; m[r+2][b]=1; m[r+3][b]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="I2"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 || m[r-1][b+2]==1 || m[r-1][b+3]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r][b+2]=1; m[r][b+3]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+
+    else if(block=="O"){
+        if(r==0 || m[r-1][b]==1 || m[r-1][b+1]==1 ){
+            m[r][b]=1; m[r][b+1]=1; m[r+1][b]=1; m[r+1][b+1]=1;
+            k=r;
+        }
+        else
+            check(m,block,b,r-1,k);
+    }
+}
+
+void deleterow(int **m, int k, int row, int col){
+    bool fullflag=true;
+    bool emptyflag=true;
+    for(int i=0;i<col;i++){
+        if(m[k][i]==0){fullflag=false;}
+        else{emptyflag=false;}
+    }
+    if(emptyflag){}
+    else if(fullflag){
+        deleterow(m, k+1, row, col);
+        for(int r=k;r<=row+2;r++)
+            m[r]=m[r+1];
+        m[row+3]={0};
+    }
+    else{deleterow(m, k+1, row, col);}
+}
+
+void determine(int **m, int row, int col, bool &exitflag){
+    for(int i=0;i<col;i++){
+        if(m[row][i]==1)
+            exitflag=true;
+    }
+}
 
 int main(){
     ifstream fin;
@@ -31,6 +216,7 @@ int main(){
     string block;
     int row, col, base, k=0;
     int **matrix;
+    bool exitflag=false;
 
     getline(cin, fname);
     fin.open(fname);
@@ -50,8 +236,11 @@ int main(){
         }
         else{
             stringstream(lines) >> block >> base;
-            check(matrix, block, base, row, k);
-            //deleterow();
+            check(matrix, block, base-1, row, k);
+            if(k==row){end(matrix,row,col);break;}
+            deleterow(matrix, k, row, col);
+            determine(matrix, row, col, exitflag);
+            if(exitflag){end(matrix,row,col);break;}
         }
     }
 
